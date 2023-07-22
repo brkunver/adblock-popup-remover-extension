cc3b9c4e6a7225d1be65a65c7b7c210692d6723f
burakhanunver <burakhanunver@gmail.com>
2023-07-22 14:10:33 +0300

    fix error

--- content.js
+++ content.js
@@ -1,26 +1,26 @@
+var bodyElement = document.querySelector("body")
+
 function removePopup() {
-  var bodyElement = document.querySelector("body")
   bodyElement.classList.remove("mdpDeblocker-style-compact")
   bodyElement.classList.remove("mdpDeblocker-blur")
   console.log("popup removed")
 }
-
 
 window.addEventListener("load", function () {
   removePopup()
 
   var observer = new MutationObserver(function (mutationsList) {
     for (var mutation of mutationsList) {
-    
-      if (mutation.type === "attributes" && mutation.attributeName === "class") {
+      if (
+        mutation.type === "attributes" &&
+        mutation.attributeName === "class" &&
+        (bodyElement.classList.contains("mdpDeblocker-style-compact") ||
+          bodyElement.classList.contains("mdpDeblocker-blur"))
+      ) {
         removePopup()
       }
     }
-
-    
-    observer.disconnect()
   })
-
 
   observer.observe(document.body, { attributes: true })
 })
