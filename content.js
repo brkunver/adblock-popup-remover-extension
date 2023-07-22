7fa8d87ab92a0e85c3955cc167fd31866280c3b4
burakhanunver <burakhanunver@gmail.com>
2023-07-22 14:05:21 +0300

    edit content js

--- content.js
+++ content.js
@@ -5,23 +5,22 @@ function removePopup() {
   console.log("popup removed")
 }
 
-// Web sayfasının tamamen yüklendiğinde çalışacak olan kod
+
 window.addEventListener("load", function () {
   removePopup()
 
-  // MutationObserver oluşturulması
   var observer = new MutationObserver(function (mutationsList) {
     for (var mutation of mutationsList) {
-      // Eğer body elementine sınıf eklenmişse ve sınıfı kaldırmadıysak, sınıfı kaldıralım
+    
       if (mutation.type === "attributes" && mutation.attributeName === "class") {
         removePopup()
       }
     }
 
-    // İzlemeyi durdur
+    
     observer.disconnect()
   })
 
-  // Observer'ın başlatılması ve izlenecek değişikliklerin tanımlanması
+
   observer.observe(document.body, { attributes: true })
 })
